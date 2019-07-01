16585ca9ad7d4a41f0c1ee4b27b86d8e6bd8c224
hoangdinhtran1995 <51941188+hoangdinhtran1995@users.noreply.github.com>
2019-07-02 08:30:20 +0900

    sort submission
    This is a submission of the sort assignment in the algorithms and data structures class 2Q 2019.

--- src/quick_sort.c
+++ src/quick_sort.c
@@ -18,7 +18,28 @@ void swap(int *p, int *q){
 A[0], A[1], ..., A[n-1] をソートして昇順に書き換える関数
 */
 void quick_sort(int A[], int n){
+	if(n <= 1) 
+		return;
+	
+	int i, j, pivot;
+		
+	pivot = A[n/2];
+	A[n/2] = A[0];
+	A[0] = pivot;
+	
+	for(i = j = 1; i < n; i++){
+		if(A[i] <= pivot){
+			swap(A+i, A+j);
+			j++;
+		}
+	}
+	
+	quick_sort(A, j);
+	quick_sort(A+j, n-j);
+	
+	return;
 }
+
 
 int main(){
   int i;
@@ -27,9 +48,14 @@ int main(){
   for(i=2;i<N;i++){
     A[i] = (long long int) A[i-1] * A[1] % N;
   }
+  
+//  for(i=0;i<N;i++){
+//	  printf("%d in %d\n", A[i], i);}
 
   quick_sort(A, N);
   for(i=0;i<N;i++){
-    if(A[i] != i) printf("ERROR %dth element is %d\n", i, A[i]);
+    if(A[i] != i) 
+		printf("ERROR %dth element is %d\n", i, A[i]);
+	printf("%d in %d\n", A[i], i);
   }
 }
--- src/quick_sort_improved.c
+++ src/quick_sort_improved.c
@@ -18,6 +18,31 @@ void swap(int *p, int *q){
 A[0], A[1], ..., A[n-1] をソートして昇順に書き換える関数
 */
 void quick_sort(int A[], int n){
+	    if(n <= 1) return;
+    int i1, j1, i2, j2, pivot;
+
+// 真ん中の要素をピボットとする
+    pivot = A[n/2];
+    A[n/2] = A[0];
+    A[0] = pivot;
+    for(i1 = j1 = 1; i1 < n; i1++){
+        if(A[i1] <= pivot){
+            swap(A+i1, A+j1);
+            j1++;
+        }
+    }
+	
+    for(i2 = j2 = 1; i2 < j1; i2++){
+        if(A[i2] < pivot){
+            swap(A+i2, A+j2);
+            j2++;
+        }
+    }
+
+    quick_sort(A, j2);
+    quick_sort(A+j1, n-j1);
+
+    return;
 }
 
 int main(){
@@ -33,5 +58,6 @@ int main(){
   quick_sort(A, N);
   for(i=0;i<N;i++){
     if(A[i] != i) printf("ERROR %dth element is %d\n", i, A[i]);
+  printf("%d in %d\n", A[i], i);
   }
 }
